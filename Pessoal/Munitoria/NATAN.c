40c691f028f0ecc6ae1928957d1cc41ee82005dd
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2025-09-09 19:48:29 -0400

    09-09-2025_19-48-29-Cassiano

--- Pessoal/Munitoria/NATAN.c
+++ Pessoal/Munitoria/NATAN.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 
 int main(){
-    int estudantes;
-    printf("Digite a quantidade de estudantes: ");
-    scanf("%d", &estudantes);
+    int estudantes,i=1;
+    while (1){
+        scanf(" %d", &estudantes);
+        if(estudantes == 0) break;
+        printf("\n------CASO DE TESTE %d------\n",i);
+        int acertos[estudantes];
+        char caracteres[estudantes][20];
+        for (int i = 0; i < estudantes; i++) {
+            acertos[i] = 0; 
+        }
+        char gabarito[20] = {'A', 'E', 'B', 'D', 'E', 'A', 'B', 'D', 'E', 'D', 'A', 'E', 'B', 'D', 'E', 'A', 'B', 'D', 'E', 'D'};
 
-    int acertos[estudantes];
-    char caracteres[estudantes][20];
-    for (int i = 0; i < estudantes; i++) {
-        acertos[i] = 0; 
-    }
-
-    char gabarito[20] = {'A', 'E', 'B', 'D', 'E', 'A', 'B', 'D', 'E', 'D', 'A', 'E', 'B', 'D', 'E', 'A', 'B', 'D', 'E', 'D'};
-
-    for(int i = 0; i < estudantes; i++){
-        printf("Respostas do Estudante %d (digite 20 caracteres separados por espaco):\n", i + 1);
-        for(int j = 0; j < 20; j++){
-            scanf(" %c", &caracteres[i][j]);
-            if (caracteres[i][j] == gabarito[j]){
-                acertos[i] = acertos[i] + 1;
+        for(int i = 0; i < estudantes; i++){
+            for(int j = 0; j < 20; j++){
+                scanf(" %c", &caracteres[i][j]);
+                if (caracteres[i][j] == gabarito[j]){
+                    acertos[i] = acertos[i] + 1;
+                }
             }
         }
+        
+        printf("\n--- Resultados ---\n");
+        for(int i = 0; i < estudantes; i++){
+            printf("\nO estudante %d teve %d acertos.\n", i + 1, acertos[i]);
+            printf("Respostas do estudante:\n");
+            for(int j = 0; j < 20; j++){
+                printf("%c ", caracteres[i][j]);
+            }
+            printf("\n");
+        }
+        i++;
     }
     
-    printf("\n--- Resultados ---\n");
-    for(int i = 0; i < estudantes; i++){
-        printf("\nO estudante %d teve %d acertos.\n", i + 1, acertos[i]);
-        printf("Respostas do estudante:\n");
-        for(int j = 0; j < 20; j++){
-            printf("%c ", caracteres[i][j]);
-        }
-        printf("\n");
-    }
 
     return 0;
 }
