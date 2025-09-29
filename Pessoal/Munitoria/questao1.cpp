8191b6e732eac9da9e5aedb80a4c54488b2f6571
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2025-09-29 08:40:59 -0400

    29-09-2025_08-40-59-Cassiano

--- Pessoal/Munitoria/questao1.cpp
+++ Pessoal/Munitoria/questao1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//Imprime o vetor normalmento 
 void Imprimir(int *v,int tam){
     for(int i=0;i < tam;i++){
         printf("%d,",v[i]);
@@ -9,25 +9,58 @@ void Imprimir(int *v,int tam){
     printf("%d\n",v[tam]);
 }
 
+//Usada para imrimir o vetor inveritdo 
 void Imprimir_Invetido(int *v,int tam){
-    for(int i=tam-1;i > 0;i--){
+    for(int i=tam;i > 0;i--){
         printf("%d,",v[i]);
     }
     printf("%d\n",v[0]);
 }
 
-int* Tranfere_vetor(int *v,int *t,int tam){
+//Criando um novo vetor e colocando os valores la dentro
+int* Tranfere_vetor(int *v,int tam,int soma){
+    int *temp = (int *)malloc(soma*sizeof(int));
     for(int i=0;i < tam;i++){
-        t[i] = v[i];
+        temp[i] = v[i];
     }
     free(v);
+    return temp;
+}
+
+//Usando a realocaçao de sempre aumentando o tamanho tamanho do vetor
+int* Realocacao(int *v,int tam,int soma){
+    int *t = (int *)realloc(v,soma*sizeof(int));
     return t;
 }
+
+//Faz a verificaçao para ve se ele gerou mesmo o vetor coretamente
+void Verifica_Resposta(int *v,int *resposta,int tam){
+    for(int i=0;i<tam;i++){
+        if(v[i] != resposta[i]){
+            printf("Os vetores sao diferentes!!!\n");
+            return;
+        }
+    }
+    printf("Os vetores sao iguais!!!!\n");
+}
+
 
 int main(){
     FILE *vetores;
     //char *nome_arquivo = "questao1.txt";
     int *vetor,n,total = 0,in;
+    int Resposta[] = {
+    85, 14, 63, 28, 97, 10, 52, 73, 31, 44, 19, 90,
+    66, 13, 54, 27, 32, 89, 42,
+    11, 95, 22, 38, 77, 40, 58, 74, 9, 83, 61, 5, 48, 20, 99,
+    71, 3, 84, 29, 55, 60,
+    92, 14, 39, 65, 2, 88, 53, 7, 96, 30,
+    18, 77, 23, 44, 11, 95, 36, 62,
+    41, 59, 86, 17, 68, 4, 35, 72, 19, 50, 8, 91, 25,
+    99, 33, 71, 46, 12, 64, 58, 21, 87,
+    26, 79, 8, 95, 62, 34, 47, 70, 18, 53, 5
+    };
+
 
     //Fazer a leitura do arquivo
     vetores = fopen("questao1.txt","r");
@@ -42,30 +75,29 @@ int main(){
     }
     //printf("O valor de n: %d\n",n);
     total += n;
+    //printf("Total = %d\n",total);
+    //Imprimir(vetor,total);
     while (in != EOF){
         in = fscanf(vetores,"%d",&n);
-        if(in == EOF) break;
         //printf("O valor de n: %d\n",n);
         //printf("Total = %d\n",total);
-        //int *temp = (int *)realloc(vetor,total+n);
         int soma  = total+n;
-        int *temp = (int *)malloc(soma*sizeof(int));
-        if(temp == NULL){
-            printf("Nao foi posivel ser realocado o vetor!!!");
-            return 1;
-        }
-        vetor = Tranfere_vetor(vetor,temp,total);
-        //printf("Total2 = %d\n",total+n);
-        for(int i=total;i < n+total;i++){
-            //printf("%d ",i);
+        //vetor = Tranfere_vetor(vetor,total,soma);
+        vetor = Realocacao(vetor,total,soma);
+        //printf("Total2 = %d\n",soma);
+        if(in == EOF) break;
+        for(int i=total;i < soma;i++){
             in = fscanf(vetores,"%d",&vetor[i]);
+            //printf("%d ",vetor[i]);
         }
         //printf("\n");
-        total += n;
+        total = soma;
         //Imprimir(vetor,total);
     }
     fclose(vetores);
     Imprimir_Invetido(vetor,total);
+    Verifica_Resposta(vetor,Resposta,total);
+    Imprimir(vetor,total);
     
     return 0;
 }
